faa357d6759c11841f45de04e30cb47aa4682d3a
luucasorfer <lucas.fernandes.94.lf@gmail.com>
2022-06-01 14:26:32 -0300

    Finalizado arrayPares

---
+++ Modulo III/variaveis-e-tipos/Desafios/arrayPares.js
@@ -0,0 +1,13 @@
+let array = [];
+
+function substituiPares(array) {
+  if (!array) array = -1;
+  if (!array.length) array = -1;
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === 0) console.log("Valor é 0");
+      else if (array[i] % 2 === 0) array[i] = 0;
+    }
+  console.log(array);
+}
+substituiPares(array)
