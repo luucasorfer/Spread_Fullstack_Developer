ac64fa89c3d9680dbe2bac1373aa5e26dcbe2635
luucasorfer <lucas.fernandes.94.lf@gmail.com>
2022-06-01 10:57:13 -0300

    Finalizado Palíndromo

--- Modulo III/variaveis-e-tipos/Desafios/palindromo.js
+++ Modulo III/variaveis-e-tipos/Desafios/palindromo.js
@@ -1,10 +1,21 @@
-
+//Solução 1
 function verificaPalindromo(string) {
-  if (!string) return "String inexistente";
+  if (!string) return "Nenhum dado recebido";
 
   console.log(
-    string.split("").reverse().join("") === string
-    );
+    string.toLowerCase().split("").reverse().join("") === string.toLowerCase() ? "Palíndromo encontrado!" : "Não é um palíndromo"
+  );
 }
+verificaPalindromo("Radar")
 
-verificaPalindromo("radar")
+//---
+
+//Solução 2
+function verificaPalindromo2(string) {
+  if (!string) return "Nenhum dado recebido";
+
+  for (let i = 0; i < string.length / 2; i++)
+    if (string[i] !== string[string.length - 1 - i]) console.log("Não é um palíndromo");
+  console.log("Palíndromo encontrado!");
+}
+verificaPalindromo2("radar");
